b2c2a0b8f611bb5c4d78fded660cfce823ff9016
EmilianStoyanov <EmilianStoyanov@github.com>
2020-11-21 00:00:17 +0200

    configure contact.html

--- python_blog/blog/urls.py
+++ python_blog/blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
+from blog import views
 from blog.views import home, about
 
 urlpatterns = [
-    path('', home, name='home-page'),
-    path('about/', about, name='about-page'),
+    path('', views.home, name='home-page'),
+    path('about/', views.about, name='about-page'),
+    path('contact/', views.contact, name='contact-page'),
 
 ]
--- python_blog/blog/views.py
+++ python_blog/blog/views.py
@@ -13,3 +13,7 @@ def home(request):
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
+
+
+def contact(request):
+    return render(request, 'blog/contact.html')
