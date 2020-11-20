777626e665ec9e211f0955d3f1d6d27a1955c40b
EmilianStoyanov <EmilianStoyanov@github.com>
2020-11-20 16:37:46 +0200

    database connection, create superuser
    connection to a database, added Post model, registered model Post, visualization of the information from the posts

--- python_blog/blog/views.py
+++ python_blog/blog/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 
-
 # Create your views here.
+from blog.models import Post
 
 
 def home(request):
-    return render(request, 'home.html')
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return render(request, 'about.html', {'title': 'About'})
+    return render(request, 'blog/about.html', {'title': 'About'})
--- python_blog/python_blog/settings.py
+++ python_blog/python_blog/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'e3iz_cmsa4r^7+begt593n_(dab=#sqs)%n2pl0i_q_6b!nqx+'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -72,17 +70,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'python_blog.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'python_blog',
+        'USER': 'postgres',
+        'PASSWORD': 'emo123',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -102,7 +102,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -115,7 +114,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
