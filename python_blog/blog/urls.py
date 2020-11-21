c0f5569c4fa6239efb1a74fc60e6c9106fe8d735
EmilianStoyanov <EmilianStoyanov@github.com>
2020-11-21 22:14:41 +0200

    added contact_final.html,  added button News

--- python_blog/blog/urls.py
+++ python_blog/blog/urls.py
@@ -6,6 +6,7 @@ from blog.views import home, about
 urlpatterns = [
     path('', views.home, name='home-page'),
     path('about/', views.about, name='about-page'),
-    path('contact/', views.contact, name='contact-page'),
+    path('contact/', views.contactUsers, name='contact-page'),
+    path('news/', views.news, name='news-page'),
 
 ]
--- python_blog/blog/views.py
+++ python_blog/blog/views.py
@@ -15,7 +15,11 @@ def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
 
 
-def contact(request):
+def news(request):
+    return render(request, 'blog/news.html')
+
+
+def contactUsers(request):
     if request.method == 'POST':
         contact = Contact()
         name = request.POST.get('name')
@@ -25,5 +29,5 @@ def contact(request):
         contact.email = email
         contact.subject = subject
         contact.save()
-        return render(request, 'blog/home.html')
+        return render(request, 'blog/contact_final.html')
     return render(request, 'blog/contact.html')
