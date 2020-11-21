ad6e41286388f3a343e8c16ee203bb9655102602
EmilianStoyanov <EmilianStoyanov@github.com>
2020-11-22 00:01:38 +0200

    added news with RSS feeds

--- python_blog/blog/views.py
+++ python_blog/blog/views.py
@@ -16,7 +16,15 @@ def about(request):
 
 
 def news(request):
-    return render(request, 'blog/news.html')
+    import requests
+    import json
+
+    news_api_request = requests.get(
+        'http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=759a41ef0f944f91a5939ea831376dd8')
+
+    api = json.loads(news_api_request.content)
+
+    return render(request, 'blog/news.html', {'api': api})
 
 
 def contactUsers(request):
