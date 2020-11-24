c9563ca011be6c575e7d8fb272b762cd862ab3c1
EmilianStoyanov <emilian.stoyanov@outlook.com>
2020-11-24 15:59:56 +0200

    creating login and logout

--- python_blog/users/views.py
+++ python_blog/users/views.py
@@ -11,6 +11,8 @@ def register(request):
         if form.is_valid():
             username = form.cleaned_data.get('username')
             form.save()
+            messages.success(request, f'Your account has been created! You are now able to log in')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
