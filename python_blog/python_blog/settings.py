b4570c20db8f005bc584da9fa36d70681ff6ae1c
EmilianStoyanov <emilian.stoyanov@outlook.com>
2020-12-07 14:00:12 +0200

    change user password

--- python_blog/python_blog/settings.py
+++ python_blog/python_blog/settings.py
@@ -130,3 +130,10 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'home-page'
 LOGIN_URL = 'login'
+
+
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = "emilian.e.stoyanov@gmail.com"
+EMAIL_HOST_PASSWORD = 'fzfgdpthbjdgrqxm'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
