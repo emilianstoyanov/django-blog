4a4f6f60bcee626e27dcefa5396907ee43de9483
Emilian Stoyanov <68276889+EmilianStoyanov@users.noreply.github.com>
2020-12-07 14:32:20 +0200

    Update settings.py

--- python_blog/python_blog/settings.py
+++ python_blog/python_blog/settings.py
@@ -133,7 +133,7 @@ LOGIN_URL = 'login'
 
 
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = "emilian.e.stoyanov@gmail.com"
-EMAIL_HOST_PASSWORD = 'fzfgdpthbjdgrqxm'
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
